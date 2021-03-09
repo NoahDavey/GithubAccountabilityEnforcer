c1078019a9aa41abcd881c19ebfb9a332bae14f3
Noah Davey <noah.davey@gmail.com>
2021-03-09 21:15:17 +1000

    Create webhook functionality

--- scripts/getRepoDetails.js
+++ scripts/getRepoDetails.js
@@ -30,6 +30,36 @@ async function main() {
     //     console.log(commit.commit.message);
     // }
 
+    // const result = await request('users/NoahDavey/repos')
+    // // console.log(result.data);
+
+    // for(let repo of result.data) {
+    //     console.log('Repository: ' + repo.name);
+    //     console.log('Hook URL: ' + repo.hooks_url);
+
+    //     // // Get repositories hooks
+    //     // const hooks = await axios.get(repo.hooks_url, {
+    //     //     headers: { 'Authorization': `Token ${process.env.GITHUB_ACCESS_TOKEN}` }
+    //     // })
+    //     // console.log(hooks.data);
+    // }
+
+    // Create webhook
+    const result = await axios.post(
+        'https://api.github.com/repos/NoahDavey/Exercism/hooks', 
+        {
+            config: {
+                url: 'https://1b8ec7699160.ngrok.io/testwebhook'                
+            }
+        },
+        {
+            headers: { 'Authorization': `Token ${process.env.GITHUB_ACCESS_TOKEN}` }
+        }
+    )
+
+    console.log(result);
+
+
     console.log('<<<<<<<<<<<<<< Got result');
 }
 
