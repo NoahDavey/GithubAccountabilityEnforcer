0124caca75af00f928a7f13fefe3344d347f1afc
Noah Davey <noah.davey@gmail.com>
2021-03-09 20:56:36 +1000

    Setup telegram webhook

--- scripts/setupTelegramWebhook.js
+++ scripts/setupTelegramWebhook.js
@@ -1,28 +1,29 @@
 require('dotenv').config()
 const axios = require('axios')
-// const TELEGRAM_WEBHOOK_IP = process.env.TELEGRAM_WEBHOOK_IP 
-const TELEGRAM_WEBHOOK_IP = " " 
+const TELEGRAM_WEBHOOK_IP = process.env.TELEGRAM_WEBHOOK_IP 
+const TELEGRAM_API_URL = process.env.TELEGRAM_API_URL
 
-async function main() {
-    console.log('About to make request >>>>>>>>>>>>>>>>');
-
-    const TELEGRAM_API = 'https://api.telegram.org'
-    // try {
-    //     await axios.post(`${TELEGRAM_API}/bot${process.env.TELEGRAM_BOT_AUTH_TOKEN}/setWebhook`, 
-    //             {url: `${TELEGRAM_WEBHOOK_IP}/telegram` },
-    //             { headers: {'content-type': 'application/json'}})
-    // } catch (error) {
-    //     console.log('---------------FAILED');
-    //     console.log(error);
-    // }
-
+async function deleteWebhook() {
     try {
-        await axios.post(`${TELEGRAM_API}/bot${process.env.TELEGRAM_BOT_AUTH_TOKEN}/deleteWebhook`)    
+        await axios.post(`${TELEGRAM_API_URL}/bot${process.env.TELEGRAM_BOT_AUTH_TOKEN}/deleteWebhook`)    
     } catch (error) {
         console.log(error);
     }
-            
-    console.log('<<<<<<<<<<<<<< Got result');
+}
+
+async function main() {
+    console.log('About to make request to telegram >>>>>>>>>>>>>>>>');
+
+    try {
+        await axios.post(`${TELEGRAM_API_URL}/bot${process.env.TELEGRAM_BOT_AUTH_TOKEN}/setWebhook`, 
+                {url: TELEGRAM_WEBHOOK_IP },
+                { headers: {'content-type': 'application/json'}})
+    } catch (error) {
+        console.log('Failed to set telegram webhook with error: ')
+        console.log(error);
+    }
+
+    console.log('<<<<<<<<<<<<<< Got Response');
 }
 
 main()
