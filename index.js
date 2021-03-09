9c6bcd9b4d5d5095b29b9a6d9aa26a1850a3fdca
Noah Davey <noah.davey@gmail.com>
2021-03-09 21:15:36 +1000

    Add bot response ability via webhook

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const { sendTelegramMessage } = require('./sendTelegramMessage')
+const axios = require('axios')
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -26,6 +27,45 @@ app.post('/github', (req, res) => {
       message += '\n'
     }
     sendTelegramMessage(message)
+
+    res.status(200).send()
+})
+
+
+app.post('/telegram', (req, res) => {
+    console.log('Recieved message from telegram');
+    console.log(req.body);
+
+    const payload = req.body 
+    const chatId = payload.message.chat.id
+    const name = payload.message.chat.username
+    console.log('Text', payload.message.text);
+    console.log('--UpdateID: ', payload.update_id);
+
+    const requestData = {
+        chat_id: chatId,
+        text: 'Hi There testing keyboard',
+        reply_to_message_id: true,
+        reply_markup: {
+            keyboard: [['Test 1'], ['Test 2'], ['Test 3']],
+            one_time_keyboard: true
+        }
+    }
+
+    const result = axios.post(
+        `${process.env.TELEGRAM_API_URL}/bot${process.env.TELEGRAM_BOT_AUTH_TOKEN}/sendMessage`,
+        requestData,
+        { headers: {'content-type': 'application/json'} }
+    )
+
+
+    res.status(200).send()
+})
+
+
+app.post('/testwebhook', (req, res) => {
+    console.log('Request was made to test webhook!');
+    console.log(req.body);
 
     res.status(200).send()
 })
